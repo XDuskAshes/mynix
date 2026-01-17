874fb1d29bc880ce9b790285b8e23fa79a7e0725
XDuskAshes <thegaymeradummy@gmail.com>
2026-01-17 16:27:19 -0500

    testing flakes

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+    # following along with a video by LibrePhoenix: https://www.youtube.com/watch?v=ACybVzRvDhs
+    description = "Test flake";
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-unstable"; # 90% just because im used to arch being rolling. love me some rolling release.
+    };
+    outputs = { self, nixpkgs, ... }:
+    let
+        lib = nixpkgs.lib;
+    in {
+        nixosConfigurations = {
+            nixtester = lib.nixosSystem {
+                system = "x86_64-linux";
+                modules = [ ./configuration.nix ];
+            }
+        };
+    };
+}
