7273558620329bcbd34ddee619601c15a770c7e1
XDuskAshes <thegaymeradummy@gmail.com>
2026-01-19 23:14:07 -0500

    [mod:spotify] PLEASE. PLEASE. PLEASE. PLEASE. x2

--- modules/apps/media/spotify.nix
+++ modules/apps/media/spotify.nix
@@ -1,19 +1,17 @@
-{ pkgs, inputs, ... }:
+{ config, pkgs, spicetify-nix, ... }:
 
-{
-    import = [ inputs.spicetify-nix.nixosModules.spicetify ];
+let
+    spicePkgs = spicetify-nix.legacyPackages.${pkgs.system};
+in {
+    imports = [ spicetify-nix.nixosModules.spicetify ];
+
     programs.spicetify = {
-        let
-            spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-        in
-        {
-            enable = true;
-            theme = spicePkgs.themes.onepunch;
-            enabledExtensions with spicePkgs.extensions; [
-                adblockify
-                shuffle
-                volumePercentage
-            ];
-        }
+        enable = true;
+        theme = spicePkgs.themes.onepunch;
+        enabledExtensions = with spicePkgs.extensions; [
+            adblockify
+            shuffle
+            volumePercentage
+        ];
     };
-}
+};
