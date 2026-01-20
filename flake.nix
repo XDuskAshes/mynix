fd5668e2f2a96f79ab5eb3845bc3522594b90193
XDuskAshes <thegaymeradummy@gmail.com>
2026-01-19 22:49:10 -0500

    [flake.nix] trying to do spotify

--- flake.nix
+++ flake.nix
@@ -3,15 +3,34 @@
     description = "Dusk's NixOS Config Flake";
     inputs = {
         nixpkgs.url = "nixpkgs/nixos-unstable"; # 90% just because im used to arch being rolling. love me some rolling release.
+        spicetify-nix.url = "github:Gerg-L/spicetify-nix";
     };
     outputs = { self, nixpkgs, ... }:
     let
         lib = nixpkgs.lib;
+        pkgs = import nixpkgs { inherit system; };
+        spicePkgs = spicetify-nix.legacyPackages.${system};
     in {
         nixosConfigurations = {
             nixtester = lib.nixosSystem {
                 system = "x86_64-linux";
-                modules = [ ./configuration.nix ];
+                modules = [
+                    ./configuration.nix
+
+                    spicetify-nix.nixosModules.default
+                ];
+                configuration = {
+                    nixpkgs.config.allowUnfree = true;
+
+                    programs.spicetify = {
+                        enable = true;
+                        enabledExtensions = with spicePkgs.extensions; [
+                           adblockify
+                           shuffle
+                           volumePercentage
+                        ];
+                    };
+                };
             };
         };
     };
