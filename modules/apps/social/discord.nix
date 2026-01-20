35a9e8153b00ac08ba8eee6e20a71a9362fbbd90
XDuskAshes <thegaymeradummy@gmail.com>
2026-01-19 23:41:16 -0500

    [mod:discord] add discord

---
+++ modules/apps/social/discord.nix
@@ -0,0 +1,10 @@
+{ config, lib, pkgs, ... }:
+
+{
+    environment.systemPackages = with pkgs; [
+        (discord.override {
+            withOpenASAR = true;
+            withVencord = true;
+        })
+    ];
+}
