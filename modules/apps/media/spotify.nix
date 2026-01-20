ac75d09130d63e69762b5f4431d19b3c7f5b1835
XDuskAshes <thegaymeradummy@gmail.com>
2026-01-19 22:57:40 -0500

    [mod:spotify] add spotify to apps

---
+++ modules/apps/media/spotify.nix
@@ -0,0 +1,19 @@
+{ pkgs, inputs, ... }:
+
+{
+    import = [ spicetify-nix.nixosModules.spicetify ];
+
+    let
+        spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+    in {
+        programs.spicetify = {
+            enable = true;
+            theme = spicePkgs.themes.onepunch;
+            enabledExtentions = with spicePkgs.extensions; [
+                adblockify
+                shuffle
+                volumePercentage
+            ];
+        };
+    };
+}
