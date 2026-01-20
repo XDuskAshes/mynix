eca2ff6524774a87ccfaa111ff46f949fde79295
XDuskAshes <thegaymeradummy@gmail.com>
2026-01-19 22:59:38 -0500

    [mod:spotify] jesus christ this documentation is not clear to me

--- modules/apps/media/spotify.nix
+++ modules/apps/media/spotify.nix
@@ -5,15 +5,14 @@
 
     let
         spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-    in {
-        programs.spicetify = {
-            enable = true;
-            theme = spicePkgs.themes.onepunch;
-            enabledExtentions = with spicePkgs.extensions; [
-                adblockify
-                shuffle
-                volumePercentage
-            ];
-        };
+    in
+    programs.spicetify = {
+        enable = true;
+        theme = spicePkgs.themes.onepunch;
+        enabledExtentions = with spicePkgs.extensions; [
+            adblockify
+            shuffle
+            volumePercentage
+        ];
     };
 }
