efcc002e91cab53740693a0d0339ac0fec06d0a1
XDuskAshes <thegaymeradummy@gmail.com>
2026-01-26 10:26:18 -0500

    [fonts.nix] more fonts, no more blank squares

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -8,6 +8,21 @@
             noto-fonts-emoji-blob-bin
             liberation_ttf
             nerd-fonts.hack
+            _0xproto
+            adwaita-fonts
+            noto-fonts
+            noto-fonts-cjk-sans
+            noto-fonts-color-emoji
+            ibm-plex
+            inconsolata
+            nerd-fonts.symbols-only
+            maple-mono.variable
+            departure-mono
+            monaspace
+            cozette
+            atkinson-hyperlegible-next
+            atkinson-hyperlegible-mono
+            corefonts
         ];
         fontDir.enable = true;
         fontconfig = {
