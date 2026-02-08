f101ae4f093653e6abe0845c2d6f2c7ef591e89e
XDuskAshes <thegaymeradummy@gmail.com>
2026-02-08 16:16:53 -0500

    discord, again.

---
+++ modules/apps/social/discord.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }:
+
+{
+    environment.systemPackages = with pkgs; [
+        (discord.override{
+            withVencord = true;
+            withOpenASAR = true;
+        })
+    ];
+}
