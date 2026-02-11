adb3d578b8a76df263e61d86ddeb2353878f3e54
XDuskAshes <thegaymeradummy@gmail.com>
2026-02-11 18:23:08 -0500

    [home.nix] add return youtube dislike + comments

--- home.nix
+++ home.nix
@@ -38,26 +38,45 @@
             ExtensionSettings = let
                 moz = short: "https://addons.mozilla.org/firefox/downloads/latest/${short}/latest.xpi";
             in {
-                "uBlock0@raymondhill.net" = {
+                # Not perfect, but it's something.
+                "uBlock0@raymondhill.net" = { # uBlock Origin - https://ublockorigin.com/
                     install_url = moz "ublock-origin";
                     installation_mode = "force_installed";
                     default_area = "navbar";
                 };
-                "jid1-MnnxcxisBPnSXQ@jetpack" = {
+                "jid1-MnnxcxisBPnSXQ@jetpack" = { # Privacy Badger - https://privacybadger.org/
                     install_url = moz "privacy-badger17";
                     installation_mode = "force_installed";
                     default_area = "navbar";
                 };
-                "@contain-facebook" = {
+                
+                # i feel like a lot of misinformation among the elderly could be solved by deleting facebook
+                # probably anyway
+                "@contain-facebook" = { # Facebook Container - https://github.com/mozilla/contain-facebook
                     install_url = moz "facebook-container";
                     installation_mode = "force_installed";
                 };
-                "sponsorBlocker@ajay.app" = {
+
+                # YouTube by itself is a clusterfuck that basically requires
+                # you to yank your wallet out and pay for convenience. While
+                # I agree paying for convenience is sometimes okay, like
+                # paying some other dude to fix your plumbing, I don't think
+                # you should have to pay for what feels like a usable experience.
+                # 
+                # As unfortunate as this is, YouTube as it is remains too big to fail.
+                # That being that it can't fail, or else it takes basically most of
+                # modern human knowledge down with it into the smoldering pit it would
+                # become. So we have to work around the greedy bullshit.
+                "sponsorBlocker@ajay.app" = { # SponsorBlock - https://sponsor.ajay.app/
                     install_url = moz "sponsorblock";
                     installation_mode = "force_installed";
                 };
-                "{34daeb50-c2d2-4f14-886a-7160b24d66a4}" = {
+                "{34daeb50-c2d2-4f14-886a-7160b24d66a4}" = { # Youtube-shorts Block - https://github.com/doma-itachi/Youtube-shorts-block
                     install_url = moz "youtube-shorts-block";
+                    installation_mode = "force_installed";
+                };
+                "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = { # Return Youtube Dislike - https://returnyoutubedislike.com/
+                    install_url = moz "return-youtube-dislikes";
                     installation_mode = "force_installed";
                 };
             };
