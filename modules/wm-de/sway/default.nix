e228a4a3f1fc8ea63428c9f64e950fb1c46a2c47
XDuskAshes <thegaymeradummy@gmail.com>
2026-02-12 18:00:29 -0500

    [mod:sway] absolute clusterfuck to get working

--- modules/wm-de/sway/default.nix
+++ modules/wm-de/sway/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 let
     cfg = config.wmde.sway;
@@ -7,20 +7,26 @@ in
     options = {
         wmde.sway.enable = lib.mkOption {
             default = false;
-            type = with lib.types; bool
-            description = "Enable/Disable Sway WM."
+            type = with lib.types; bool;
+            description = "Enable/Disable Sway WM.";
         };
     };
+ 
+    imports = [
+       ../basewl.nix
+       ../waylandwmtools.nix
+    ];
 
+   
     config = lib.mkIf cfg.enable {
-
-        imports = [
-            ../basewl.nix
-            ../waylandwmtools.nix
-        ];
-
+        services.seatd = {
+            enable = true;
+        };
+        services.gnome.gnome-keyring.enable = true;
+        
         environment.systemPackages = with pkgs; [
             sway
+            seatd
         ];
 
         programs.sway = {
@@ -29,5 +35,5 @@ in
                 enable = true;
             };
         };
-    }
+    };
 }
