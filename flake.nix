be518a3ef60d89bb49f3c1e4538cc2349be5fb26
XDuskAshes <thegaymeradummy@gmail.com>
2026-02-15 22:06:21 -0500

    [flake,spotify] clusterfuck of a reorganization but i did it!

--- flake.nix
+++ flake.nix
@@ -1,51 +1,54 @@
 {
     description = "Dusk's NixOS Config Flake";
+
     inputs = {
-        nixpkgs.url = "nixpkgs/nixos-unstable"; # 90% just because im used to arch being rolling. love me some rolling release.
+        nixpkgs = {
+            url = "github:nixos/nixpkgs/nixos-unstable"; # 90% just because im used to arch being rolling. love me some rolling release.
+        };
+
         home-manager = {
             url = "github:nix-community/home-manager/release-25.11";
             inputs.nixpkgs.follows = "nixpkgs";
         };
-        spicetify-nix.url = "github:Gerg-L/spicetify-nix";
-        nvf = {
-            url = "github:notashelf/nvf";
-            inputs = {
-                nixpkgs = {
-                    follows = "nixpkgs";
-                };
 
-                home-manager = {
-                    follows = "home-manager";
-                };
-            };
+        spicetify-nix = {
+            url = "github:Gerg-L/spicetify-nix";
+            inputs.nixpkgs.follows = "nixpkgs";
         };
     };
-    outputs = { self, nixpkgs, home-manager, spicetify-nix, nvf, ... }:
-    let
-        lib = nixpkgs.lib;
-        system = "x86_64-linux";
-    in {
-        nixosConfigurations = {
-            nixie = lib.nixosSystem {
+
+    outputs =
+        {
+            self,
+            nixpkgs,
+            home-manager,
+            spicetify-nix,
+            ...
+        }@inputs:
+        {
+            nixosConfigurations.nixie = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                specialArgs = { inherit inputs spicetify-nix; };
+
                 modules = [
                     ./configuration.nix
+                    ./modules/apps/media/spotify.nix
+
                     home-manager.nixosModules.home-manager
                     {
                         home-manager = {
-                            useGlobalPkgs = true;
-                            useUserPackages = true;
-                            users.dusk = import [
+                        useUserPackages = true;
+                        backupFileExtension = "home-manager_backup";
+                        extraSpecialArgs = { inherit inputs; };
+                        users.dusk = {
+                            imports = [
                                 ./home.nix
-                                nvf.homeManagerModules.default
+                                inputs.nvf.homeManagerModules.default
                             ];
-                            backupFileExtension = "backup";
                         };
-                    }
-                    ./modules/apps/media/spotify.nix
-                ];
-                specialArgs = { inherit spicetify-nix; };
-            };
+                    };
+                }
+            ];
         };
     };
 }
-
--- modules/apps/media/spotify.nix
+++ modules/apps/media/spotify.nix
@@ -1,20 +1,18 @@
 { config, pkgs, spicetify-nix, ... }:
 
 let
-  spicePkgs = spicetify-nix.legacyPackages.${pkgs.system};
+    spicePkgs = spicetify-nix.legacyPackages.${pkgs.system};
 in {
-  imports = [
-    spicetify-nix.nixosModules.spicetify
-  ];
-
-  programs.spicetify = {
-    enable = true;
-    theme = spicePkgs.themes.onepunch;
-    enabledExtensions = with spicePkgs.extensions; [
-      adblockify
-      shuffle
-      volumePercentage
+    imports = [
+        spicetify-nix.nixosModules.spicetify
     ];
-  };
+    programs.spicetify = {
+        enable = true;
+        theme = spicePkgs.themes.onepunch;
+        enabledExtensions = with spicePkgs.extensions; [
+            adblockify
+            shuffle
+            volumePercentage
+        ];
+    };
 }
-
