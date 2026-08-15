9454dd8db86865fb947fa5b1f3278e1f9fcdc0de
XDuskAshes <xduskashesreal@gmail.com>
2026-08-15 18:27:05 -0400

    vencord, no openasar
    Signed-off-by: XDuskAshes <xduskashesreal@gmail.com>

--- modules/apps/discord.nix
+++ modules/apps/discord.nix
@@ -5,8 +5,8 @@
     {
       environment.systemPackages = with pkgs; [
         (discord.override {
-          withVencord = false; # 06-12-2026: Package is fucked!
-          withOpenASAR = true; # 06-12-2026: Package is fixed!
+          withVencord = true;
+          withOpenASAR = false;
         })
       ];
     };
